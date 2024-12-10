ccd893992f0f4b9296983597b368a2d8c9c2e99e
Ect61 <59390777+Ect61@users.noreply.github.com>
2024-12-10 21:51:50 +0300

    Update

--- metro.config.js
+++ metro.config.js
@@ -1,8 +1,15 @@
 const { getDefaultConfig, mergeConfig } = require("@react-native/metro-config");
 const { withNativeWind } = require("nativewind/metro");
+const { wrapWithReanimatedMetroConfig } = require("react-native-reanimated/metro-config");
 
-const config = mergeConfig(getDefaultConfig(__dirname), {
+let config = mergeConfig(getDefaultConfig(__dirname), {
   /* your config */
 });
 
-module.exports = withNativeWind(config, { input: "./global.css" });
+// NativeWind ile yapılandır
+config = withNativeWind(config, { input: "./global.css" });
+
+// Reanimated ile yapılandır
+config = wrapWithReanimatedMetroConfig(config);
+
+module.exports = config;
